0ab031257b0ba701e42aa63d889b9ed9a533e69d
Artem Oganesyan <artem.oganesyan@gmail.com>
2024-01-31 10:16:40 -0800

    Project fix

--- src/test/java/definitions/MandrykStepDefs.java
+++ src/test/java/definitions/MandrykStepDefs.java
@@ -6,14 +6,14 @@ import support.Helper;
 import java.io.IOException;
 import java.sql.SQLException;
 
-public class MandrykStepDefs implements En {
-
-    @Then("I confirm user account with email {string}")
-    public void iConfirmUserAccountWithEmail(String email) throws SQLException, IOException {
-        String activationString = Helper.getAccessToken(email);
-        String[] str = activationString.split(";");
-        int userId = Integer.parseInt(str[0]);
-        String activationCode = str[1];
-        Helper.activateUser(userId, activationCode);
-    }
-}
+//public class MandrykStepDefs implements En {
+//
+//    @Then("I confirm user account with email {string}")
+//    public void iConfirmUserAccountWithEmail(String email) throws SQLException, IOException {
+//        String activationString = Helper.getAccessToken(email);
+//        String[] str = activationString.split(";");
+//        int userId = Integer.parseInt(str[0]);
+//        String activationCode = str[1];
+//        Helper.activateUser(userId, activationCode);
+//    }
+//}
