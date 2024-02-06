06b05908306fa5cb7ca7a01e5a73028b774d6f00
kiiana <anastasijakiyan@gmail.com>
2024-02-06 11:39:36 -0800

    reg1

--- src/test/java/definitions/Kiian_StepDefs.java
+++ src/test/java/definitions/Kiian_StepDefs.java
@@ -1,14 +1,16 @@
 package definitions;
+import cucumber.api.java.en.Then;
 import cucumber.api.java8.En;
 import cucumber.api.java.en.And;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-
 import static support.TestContext.getDriver;
+import support.Helper;
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class Kiian_StepDefs implements En {
-
     @And("Kiian I move question slider {int} step to the {string}")
     public void iMoveSliderStepToThe(int steps, String direction) {
         WebElement slider = getDriver().findElement(By.xpath("//*[contains(text(), 'Q1')]/../../..//mat-slider"));
@@ -20,5 +22,14 @@ public class Kiian_StepDefs implements En {
                 slider.sendKeys(Keys.ARROW_RIGHT);
             }
         }
+    }
+
+    @Then("Kiian I confirm email address {string}")
+    public void kiianIConfirmEmailAddress(String email) throws SQLException, IOException {
+        String activationString = Helper.getAccessToken(email);
+        String[] str = activationString.split(";");
+        int userId = Integer.parseInt(str[0]);
+        String activationCode = str[1];
+        Helper.activateUser(userId, activationCode);
     }
 }
